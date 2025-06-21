7811dda3f683e6c35add7633b74a9af32dd710d5
AndyyDev06 <andrewtb006@gmail.com>
2025-06-21 16:55:04 -0400

    fix: Switch to HashRouter for GitHub Pages compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router basename="/AiBud/">
+    <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/chat" element={<App />} />
